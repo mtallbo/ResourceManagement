efaec54ce86656eb0e5dff000773a4804511c8c9
mikael.tallbo <mikael.tallbo@gmail.com>
2019-12-27 10:22:02 +0100

    Fixed bugs for release

--- ITHS-DB-Lab3-Database/dbo/Stored Procedures/spCreateLoan.sql
+++ ITHS-DB-Lab3-Database/dbo/Stored Procedures/spCreateLoan.sql
@@ -15,40 +15,23 @@ Den lånar om dessa villkor fylls
 *Exemplaret finns och har ett returvärde
 */
 begin try
-	if exists(select * from Loans where ResourceEntityId = @resourceEntityId and ReturnDate is null)
-        begin
-            print 'Item not available'
-        end
-	else if not exists(select * from ResourceEntities where ResourceEntities.Id = @resourceEntityId)
-        begin
-            print 'Item does not exist'
-        end
-	else if exists(select * from ResourceEntities where ResourceEntities.EntityId = @resourceEntityId and ResourceEntities.LostByLoanId is null)
-		begin
-            print 'Item is missing'
-        end
-	else if (@loanerId = @borrowerId)
-		begin
-			print 'You cannot loan from yourself'
-		end
-	else
-		insert into
-			[dbo].[Loans]
-			(
-			[LoanerId],
-			[BorrowerId],
-			[ResourceEntityId],
-			[StartDate],
-			[EndDate]
-			)
-		values
-			(
-			@loanerId,
-			@borrowerId,
-			@resourceEntityId,
-			@startDate,
-			@endDate
+	insert into
+		[dbo].[Loans]
+		(
+		[LoanerId],
+		[BorrowerId],
+		[ResourceEntityId],
+		[StartDate],
+		[EndDate]
 		)
+	values
+		(
+		@loanerId,
+		@borrowerId,
+		@resourceEntityId,
+		@startDate,
+		@endDate
+	)
 END try
 begin catch
 	select ERROR_NUMBER(), ERROR_MESSAGE();
